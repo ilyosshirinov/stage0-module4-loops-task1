b91ae0424fd20e10e4c527ce4f17813a41f6f24e
i.shirinov <shirinovilyos6965@gmail.com>
2022-12-05 15:49:51 +0500

    kirish. 1-topshiriq: Takrorlash yomon.

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,7 +3,14 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public void repeatIsBad() {
+        for (int runner = 0; runner < 20; ++runner) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }
+    }
 
+    public static void main(String[] args) {
+        RepeatIsBad repeatIsBad = new RepeatIsBad();
+        repeatIsBad.repeatIsBad();
     }
 }
